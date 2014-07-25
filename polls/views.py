a7c023bf4e43eee5941a1cb574e66582d96e99b7
dobestan <dobestan@gmail.com>
2014-07-26 01:28:12 +0900

    update views.py : via dynamic template

--- polls/views.py
+++ polls/views.py
@@ -3,16 +3,26 @@ from django.http import HttpResponse
 
 from django.template import loader, RequestContext
 
+from polls.models import Question
+
 # Create your views here.
 
 def index(request):
   template = loader.get_template("polls/index.html")
+  questions = Question.objects.all()
+  context = RequestContext(request, {
+    "questions": questions
+  })
 
   # via HttpResponse
   # return HttpResponse("Welcome to Polls App")
 
   # via static template ( without python code )
-  return HttpResponse(template)
+  # return HttpResponse(template)
+
+  # via dynamic template ( with python code)
+  # but quite complex : should refactor later using render
+  return HttpResponse(template.render(context))
 
 def detail(request, question_id):
   return HttpResponse("Detail of " + question_id)
