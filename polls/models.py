5fa8dd278f258c1779236b44f1b918a4fccd593b
dobestan <dobestan@gmail.com>
2014-07-26 01:23:48 +0900

    update model : override __unicode__ method

--- polls/models.py
+++ polls/models.py
@@ -5,11 +5,15 @@ from django.db import models
 class Question(models.Model):
     title = models.CharField(max_length=100)
     pub_date = models.DateTimeField("date published")
+    def __unicode__(self):
+        return self.title
 
 class Choice(models.Model):
     question = models.ForeignKey(Question)
     title = models.CharField(max_length=20)
     votes = models.IntegerField(default=0)
+    def __unicode__(self):
+        return self.title
 
 
 """
