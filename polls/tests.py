01e1c52493fcc7ae68e096726f7a5a15f8ba6698
dobestan <dobestan@gmail.com>
2014-07-26 00:41:30 +0900

    refactoring tests.py : remove redundancy

--- polls/tests.py
+++ polls/tests.py
@@ -4,12 +4,12 @@ from django.test.client import Client
 # Create your tests here.
 
 class IndexPageTest(TestCase):
+  def setUp(self):
+    self.client = Client()
+    self.response = self.client.get('/polls/')
+
   def test_request_status_code_should_be_200(self):
-    client = Client()
-    response = client.get('/polls/')
-    self.assertEqual(response.status_code, 200)
+    self.assertEqual(self.response.status_code, 200)
 
   def test_request_body_should_contain_welcome_to_polls_app(self):
-    client = Client()
-    response = client.get('/polls/')
-    self.assertContains(response, "Welcome to Polls App")
+    self.assertContains(self.response, "Welcome to Polls App")
