889bbfc7f5d03a2bfbda3faaf3530fe4ff8012ca
dobestan <dobestan@gmail.com>
2014-07-25 23:16:18 +0900

    add Model : Question and Choice

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    title = models.CharField(max_length=100)
+    pub_date = models.DateTimeField("date published")
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question)
+    title = models.CharField(max_length=20)
+    votes = models.IntegerField(default=0)
