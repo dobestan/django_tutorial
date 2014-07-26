722cd2646d4bf3c6971298ce978874b31af0b230
dobestan <dobestan@gmail.com>
2014-07-26 13:26:19 +0900

    update views with generic view

--- polls/views.py
+++ polls/views.py
@@ -4,34 +4,42 @@ from django.core.urlresolvers import reverse
 
 from django.template import loader, RequestContext
 
+from django.views import generic
+
 from polls.models import Question
 
 # Create your views here.
 
-def index(request):
-  template = loader.get_template("polls/index.html")
-  questions = Question.objects.all()
+class IndexView(generic.ListView):
+  template_name = "polls/index.html"
+  context_object_name = "questions"
 
-  # via HttpResponse
-  # return HttpResponse("Welcome to Polls App")
+  def get_queryset(self):
+    return Question.objects.all()
 
-  # via static template ( without python code )
-  # return HttpResponse(template)
+# via HttpResponse
+# return HttpResponse("Welcome to Polls App")
 
-  # via dynamic template ( with python code)
-  # but quite complex : should refactor later using render
-  # return HttpResponse(template.render(context))
+# via static template ( without python code )
+# return HttpResponse(template)
 
-  # via django.shortcuts render
-  return render(request, "polls/index.html", { "questions": questions })
+# via dynamic template ( with python code)
+# but quite complex : should refactor later using render
+# return HttpResponse(template.render(context))
 
-def detail(request, question_id):
-  question = Question.objects.get(pk=question_id)
-  return render(request, "polls/detail.html", { "question": question })
+# via django.shortcuts render
+# return render(request, "polls/index.html", { "questions": questions })
 
-def results(request, question_id):
-  question = Question.objects.get(pk=question_id)
-  return render(request, "polls/results.html", { "question": question })
+
+class DetailView(generic.DetailView):
+  model = Question
+  template_name = "polls/detail.html"
+
+  # pk_url_kwarg = "question_id"
+
+class ResultsView(generic.DetailView):
+  model = Question
+  template_name = "polls/results.html"
 
 def vote(request, question_id):
   question = Question.objects.get(pk=question_id)
